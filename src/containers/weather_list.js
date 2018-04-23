359a4029488668e8d6119535a7a75e0246b2464b
unknown <gytpet1@gmail.com>
2018-04-23 16:07:39 +0300

    WeatherList container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class WeatherList extends Component {
+
+  render() {
+    return (
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Tempeture</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather } // { weather } == weather: state.weather
+}
+
+export default connect(mapStateToProps)(WeatherList)
